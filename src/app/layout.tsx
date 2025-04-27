896063937f8c0ae27c733322162395edc8d6a5d5
Rohit Kumar Mahto <rohitmahto062@gmail.com>
2025-04-27 15:00:33 +0530

    Add MainNavbar component and update dependencies
    - Introduced MainNavbar component for navigation.
    - Updated package.json and package-lock.json to include new dependencies: @tabler/icons-react, clsx, motion, and tailwind-merge.
    - Refactored page layout to incorporate MainNavbar.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import MainNavbar from "@/components/MainNavbar";
+// import { style } from "motion/react-client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <MainNavbar />
         {children}
       </body>
     </html>

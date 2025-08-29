7b6cd56cb88d974e81a86dfdb334583b73280376
rohit <rohitmahto062@gmail.com>
2025-08-29 17:08:22 +0530

    feat: Update ConnectMe and ServiceSection components with contact information and service offerings

--- src/components/ConnectMe.tsx
+++ src/components/ConnectMe.tsx
@@ -19,19 +19,39 @@ function ConnectMe() {
         className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
       >
         <h3 className="pt-0">💬 Let’s Connect</h3>
-        <div className="flex flex-col mt-4 text-lg text-slate-300 max-w-xl mx-auto font-sans">
-          <p className="font-mono">
-            I’m always open to exciting opportunities, collaborations, or just a
-            friendly tech chat! 🚀
+      </motion.h1>
+      <div className="flex flex-col mt-4 text-lg text-slate-300 max-w-xl mx-auto font-sans justify-center">
+        <p className="font-mono">
+          I’m always open to exciting opportunities, collaborations, or just a
+          friendly tech chat! 🚀
+        </p>
+        <div className="flex flex-col justify-center gap-2 mt-4 align-center">
+          <p>
+            <strong className="text-amber-300">📩 Email:</strong>{" "}
+            rohitmahto062@gmail.com
           </p>
           <p>
-            <strong>📩 Email:</strong> [your email]{" "}
+            <strong className="text-amber-300">💼LinkedIn: </strong>
+            <a
+              href="https://www.linkedin.com/in/rohit-kumar-mahto-b041a3248/"
+              target="_blank"
+              className="text-blue-400"
+            >
+              https://www.linkedin.com/in/rohit-kumar-mahto
+            </a>
           </p>
-          <p>💼 LinkedIn: [your LinkedIn URL]</p>
-          <p> 🐙 GitHub: [your GitHub URL]</p>
-          <p>🌐 Portfolio: [your portfolio URL]</p>
+          <p>
+            <strong className="text-amber-300">🐙 GitHub: </strong>
+            <a
+              className="text-blue-400"
+              href="https://github.com/toxic-programmer-cell"
+              target="_blank"
+            >
+              https://github.com/toxic-programmer-cell
+            </a>
+          </p>
         </div>
-      </motion.h1>
+      </div>
     </LampContainer>
   );
 }
--- src/components/ServiceSection.tsx
+++ src/components/ServiceSection.tsx
@@ -1,62 +1,129 @@
-'use client'
-import React from 'react';
+"use client";
+import React from "react";
 import { StickyScroll } from "../components/ui/sticky-scroll-reveal";
 
+// const content = [
+//     {
+//       title: "Collaborative Editing",
+//       description:
+//         "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
+//       content: (
+//         <div className="flex h-full w-full items-center justify-center bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] text-white">
+//           Collaborative Editing
+//         </div>
+//       ),
+//     },
+//     {
+//       title: "Real time changes",
+//       description:
+//         "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
+//       content: (
+//         <div className="flex h-full w-full items-center justify-center text-white">
+//           <img
+//             src="/linear.webp"
+//             width={300}
+//             height={300}
+//             className="h-full w-full object-cover"
+//             alt="linear board demo"
+//           />
+//         </div>
+//       ),
+//     },
+//     {
+//       title: "Version control",
+//       description:
+//         "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+//       content: (
+//         <div className="flex h-full w-full items-center justify-center bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] text-white">
+//           Version control
+//         </div>
+//       ),
+//     },
+//     {
+//       title: "Running out of content",
+//       description:
+//         "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+//       content: (
+//         <div className="flex h-full w-full items-center justify-center bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] text-white">
+//           Running out of content
+//         </div>
+//       ),
+//     },
+//   ];
+
 const content = [
-    {
-      title: "Collaborative Editing",
-      description:
-        "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
-      content: (
-        <div className="flex h-full w-full items-center justify-center bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] text-white">
-          Collaborative Editing
-        </div>
-      ),
-    },
-    {
-      title: "Real time changes",
-      description:
-        "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
-      content: (
-        <div className="flex h-full w-full items-center justify-center text-white">
-          <img
-            src="/linear.webp"
-            width={300}
-            height={300}
-            className="h-full w-full object-cover"
-            alt="linear board demo"
-          />
-        </div>
-      ),
-    },
-    {
-      title: "Version control",
-      description:
-        "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
-      content: (
-        <div className="flex h-full w-full items-center justify-center bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] text-white">
-          Version control
-        </div>
-      ),
-    },
-    {
-      title: "Running out of content",
-      description:
-        "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
-      content: (
-        <div className="flex h-full w-full items-center justify-center bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] text-white">
-          Running out of content
-        </div>
-      ),
-    },
-  ];
+  {
+    title: "Full-Stack Web Development",
+    description:
+      "I build robust and scalable web applications using the latest technologies like React, Node.js, and MongoDB. From crafting beautiful frontends to designing efficient backends, I ensure seamless performance and user experience.",
+    content: (
+      <div className="flex h-full w-full items-center justify-center bg-[linear-gradient(to_bottom_right,var(--indigo-500),var(--blue-500))] text-white">
+        <img
+          src="https://www.webstackacademy.com/wp-content/uploads/2023/01/fullStack-MERN.png"
+          width={300}
+          height={300}
+          className="h-full w-full object-cover"
+          alt="Full-Stack Web Development"
+        />
+      </div>
+    ),
+  },
+  {
+    title: "Modern Frontend with React & Redux",
+    description:
+      "I specialize in building interactive and high-performance user interfaces using React.js and Redux for state management. With optimized components and reusable code patterns, I deliver pixel-perfect designs that users love.",
+    content: (
+      <div className="flex h-full w-full items-center justify-center text-white">
+        <img
+          src="https://www.seekpng.com/png/full/38-381338_frontend-and-backend-development-android-hybrid-apps-development.png"
+          width={300}
+          height={300}
+          className="h-full w-full object-contain"
+          alt="Frontend Development"
+        />
+      </div>
+    ),
+  },
+  {
+    title: "Backend & API Development",
+    description:
+      "Using Node.js and Express, I create secure, scalable REST APIs and microservices. With MongoDB as the database, I ensure data consistency, performance, and a seamless connection between frontend and backend systems.",
+    content: (
+      <div className="flex h-full w-full items-center justify-center bg-[linear-gradient(to_bottom_right,var(--green-500),var(--teal-500))] text-white">
+        <img
+          src="https://png.pngtree.com/png-clipart/20240719/original/pngtree-flat-design-backend-developer-landing-page-png-image_15592866.png"
+          width={300}
+          height={300}
+          className="h-full w-full object-contain"
+          alt="Frontend Development"
+        />
+      </div>
+    ),
+  },
+  {
+    title: "End-to-End Project Delivery",
+    description:
+      "From idea to deployment, I handle the complete lifecycle of your web application. With version control via Git/GitHub and deployment strategies on modern platforms, I deliver production-ready solutions on time.",
+    content: (
+      <div className="flex h-full w-full items-center justify-center bg-[linear-gradient(to_bottom_right,var(--rose-500),var(--pink-500))] text-white">
+        <img
+          src="https://img.freepik.com/free-vector/business-project-delivery-concept-project-development-presentation-idea-project-planning-promotion-management-marketing-flat-vector-illustration_613284-3094.jpg"
+          width={300}
+          height={300}
+          className="h-full w-full object-cover"
+          alt="Frontend Development"
+        />
+      </div>
+    ),
+  },
+];
 
 function ServiceSection() {
   return (
     <div className="w-full py-4">
       <StickyScroll content={content} />
     </div>
-  )
+  );
 }
 
-export default ServiceSection
+export default ServiceSection;
